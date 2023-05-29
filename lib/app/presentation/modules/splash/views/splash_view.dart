47866b74968f7263c58b7d82e569b2fadb580c1e
Johan Castaño <jach_1997@hotmail.com>
2023-05-29 14:37:13 -0500

    Repository Pattern - parte 2
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,11 +1,38 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../../../../data/repositories_implementation/connectivity_repository_impl.dart';
+import '../../../../domain/repository/connectivity_repository.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    ConnectivityRepository connectivityRepository =
+        ConnectivityRepositoryImpl();
+    connectivityRepository.hasInternet;
+    final hasInternet = await connectivityRepository.hasInternet;
+    if (hasInternet) {
+    } else {}
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return const Scaffold(
+      body: Center(
+        child:
+            SizedBox(width: 80, height: 80, child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
