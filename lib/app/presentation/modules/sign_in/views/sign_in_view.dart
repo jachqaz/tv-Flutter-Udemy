bc5b7cd3b1c84cc8c37ce9f7e413f88d26a4260f
Johan Castaño <jach_1997@hotmail.com>
2023-06-07 16:29:07 -0500

    248. Modulo de inicio de sesión - parte 1
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/sign_in/views/sign_in_view.dart
+++ lib/app/presentation/modules/sign_in/views/sign_in_view.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
 
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
   const SignInView({super.key});
 
   @override
+  State<SignInView> createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  String _username = '', _password = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign In'),
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                onChanged: (text) {
+                  setState(() {
+                    _username = text.trim().toLowerCase();
+                  });
+                },
+                decoration: InputDecoration(hintText: 'UserName'),
+                validator: (text) {
+                  text = text?.trim().toLowerCase() ?? '';
+                  if (text.isEmpty) {
+                    return 'Invalid username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  onChanged: (text) {
+                    setState(() {
+                      _password = text.replaceAll(' ', '');
+                    });
+                  },
+                  decoration: InputDecoration(hintText: 'Password'),
+                  validator: (text) {
+                    text = text?.replaceAll(' ', '').toLowerCase() ?? '';
+                    if (text.length < 4) {
+                      return 'Invalid password';
+                    }
+                    return null;
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              Builder(builder: (context) {
+                return MaterialButton(
+                  onPressed: () {
+                    final isValid = Form.of(context).validate();
+                    if (!isValid) {}
+                  },
+                  color: Colors.blue,
+                  child: Text('Sign In'),
+                );
+              }),
+            ],
+          ),
+        ),
       ),
-    );
+    ));
   }
 }
