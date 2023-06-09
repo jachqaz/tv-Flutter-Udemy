576081b21d5475bc8af94e04dfa63e434fc2b085
Johan Castaño <jach_1997@hotmail.com>
2023-06-09 16:30:17 -0500

    258. La clase Http - parte 2
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/data/http/http.dart
+++ lib/app/data/http/http.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:http/http.dart';
+
+import '../../domain/either.dart';
 
 class Http {
   final Client _client;
@@ -7,38 +11,63 @@ class Http {
 
   Http(this._baseUrl, this._apiKey, this._client);
 
-  request(
+  Future<Either<HttpFailure, String>> request(
     String path, {
     HttpMethod method = HttpMethod.get,
     Map<String, String> headers = const {},
     Map<String, String> queryParameters = const {},
+    bool useApiKey = true,
   }) async {
-    Uri url = Uri.parse(
-      path.startsWith('http') ? path : '$_baseUrl$path',
-    );
-    if (queryParameters.isNotEmpty) {
-      url = url.replace(queryParameters: queryParameters);
-    }
-    headers = {'content-type': 'application/json', ...headers};
-    late final Response response;
-    switch (method) {
-      case HttpMethod.get:
-        response = await _client.get(url);
-        break;
-      case HttpMethod.post:
-        response = await _client.post(url, headers: headers);
-        break;
-      case HttpMethod.patch:
-        response = await _client.patch(url, headers: headers);
-        break;
-      case HttpMethod.delete:
-        response = await _client.delete(url, headers: headers);
-        break;
-      case HttpMethod.put:
-        response = await _client.put(url, headers: headers);
-        break;
+    try {
+      if (useApiKey) {
+        queryParameters = {...queryParameters, 'apiKey': _apiKey};
+      }
+      Uri url = Uri.parse(
+        path.startsWith('http') ? path : '$_baseUrl$path',
+      );
+      if (queryParameters.isNotEmpty) {
+        url = url.replace(queryParameters: queryParameters);
+      }
+      headers = {'content-type': 'application/json', ...headers};
+      late final Response response;
+      switch (method) {
+        case HttpMethod.get:
+          response = await _client.get(url);
+          break;
+        case HttpMethod.post:
+          response = await _client.post(url, headers: headers);
+          break;
+        case HttpMethod.patch:
+          response = await _client.patch(url, headers: headers);
+          break;
+        case HttpMethod.delete:
+          response = await _client.delete(url, headers: headers);
+          break;
+        case HttpMethod.put:
+          response = await _client.put(url, headers: headers);
+          break;
+      }
+      final statusCode = response.statusCode;
+      if (statusCode >= 200) {
+        return Either.right(response.body);
+      }
+      return Either.left(HttpFailure(statusCode: statusCode));
+    } catch (e) {
+      if (e is SocketException || e is ClientException) {
+        return Either.left(HttpFailure(exception: NetworkException));
+      }
+      return Either.left(HttpFailure(exception: e));
     }
   }
 }
+
+class HttpFailure {
+  final int? statusCode;
+  final Object? exception;
+
+  HttpFailure({this.statusCode, this.exception});
+}
+
+class NetworkException {}
 
 enum HttpMethod { get, post, patch, delete, put }
