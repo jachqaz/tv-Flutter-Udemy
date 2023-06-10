40adf38b4d158b870d414851a444b996d33c401a
Johan Castaño <jach_1997@hotmail.com>
2023-06-10 00:59:18 -0500

    266. El widget MultiProvider
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

---
+++ lib/app/data/repositories_implementation/account_repository_impl.dart
@@ -0,0 +1,5 @@
+import '../../domain/repository/account_repository.dart';
+
+const _key = 'sessionId';
+
+class AccountRepositoryImpl implements AccountRepository {}
--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,37 @@ import 'package:provider/provider.dart';
 import 'package:tv/app/my_app.dart';
 
 import 'app/data/http/http.dart';
+import 'app/data/repositories_implementation/account_repository_impl.dart';
 import 'app/data/repositories_implementation/authentication_repository_impl.dart';
 import 'app/data/repositories_implementation/connectivity_repository_impl.dart';
 import 'app/data/services/remote/authentication_api.dart';
 import 'app/data/services/remote/internet_checker.dart';
+import 'app/domain/repository/account_repository.dart';
 import 'app/domain/repository/authentication_repository.dart';
 import 'app/domain/repository/connectivity_repository.dart';
 
 void main() {
-  runApp(Provider<ConnectivityRepository>(
-    create: (context) => ConnectivityRepositoryImpl(
-      Connectivity(),
-      InternetChecker(),
-    ),
-    child: Provider<AuthenticationRepository>(
-      create: (context) => AuthenticationRepositoryImpl(
-          const FlutterSecureStorage(),
-          AuthenticationApi(
-            Http(
-                client: Client(),
-                baseUrl: 'https://api.themoviedb.org/3',
-                apiKey: 'f16c8506cda4e08300b9149a1ff5cd59'),
-          )),
-      child: MyApp(),
-    ),
+  runApp(MultiProvider(
+    providers: [
+      Provider<AccountRepository>(
+        create: (context) => AccountRepositoryImpl(),
+      ),
+      Provider<ConnectivityRepository>(
+          create: (context) => ConnectivityRepositoryImpl(
+                Connectivity(),
+                InternetChecker(),
+              )),
+      Provider<AuthenticationRepository>(
+        create: (context) => AuthenticationRepositoryImpl(
+            const FlutterSecureStorage(),
+            AuthenticationApi(
+              Http(
+                  client: Client(),
+                  baseUrl: 'https://api.themoviedb.org/3',
+                  apiKey: 'f16c8506cda4e08300b9149a1ff5cd59'),
+            )),
+      ),
+    ],
+    child: MyApp(),
   ));
 }
