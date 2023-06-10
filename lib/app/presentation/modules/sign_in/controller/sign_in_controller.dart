9a5444fa61f35ee527e61aa82675ef010080d3c4
Johan Castaño <jach_1997@hotmail.com>
2023-06-10 17:29:22 -0500

    274. Estados inmutables - parte 3
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
+++ lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
@@ -1,31 +1,19 @@
-import 'package:flutter/foundation.dart';
 import 'package:tv/app/presentation/modules/sign_in/controller/sign_in_state.dart';
 
-class SignInController extends ChangeNotifier {
-  bool _mounted = true;
+import '../../../global/state_notifier.dart';
 
-  SignInState _state = SignInState();
-
-  SignInState get state => _state;
-
-  bool get mounted => _mounted;
+class SignInController extends StateNotifier<SignInState> {
+  SignInController(super.state);
 
   void onUsernameChanged(String text) {
-    _state = _state.copyWith(username: text.trim().toLowerCase());
+    onlyUpdate(state.copyWith(username: text.trim().toLowerCase()));
   }
 
   void onPasswordChanged(String text) {
-    _state = _state.copyWith(password: text.replaceAll(' ', ''));
+    onlyUpdate(state.copyWith(password: text.replaceAll(' ', '')));
   }
 
   void onFetchingChanged(bool value) {
-    _state = _state.copyWith(fetching: value);
-    notifyListeners();
-  }
-
-  @override
-  void dispose() {
-    _mounted = true;
-    super.dispose();
+    state = state.copyWith(fetching: value);
   }
 }
