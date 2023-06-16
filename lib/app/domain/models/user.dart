9a5f3e11d760668ab479863aead52dc55a6640f2
Johan Castaño <jach_1997@hotmail.com>
2023-06-15 23:25:38 -0500

    Code generation - parte 2
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/domain/models/user.dart
+++ lib/app/domain/models/user.dart
@@ -1,17 +1,18 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'user.g.dart';
+
+@JsonSerializable()
 class User extends Equatable {
   final int id;
   final String username;
 
   User({required this.id, required this.username});
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json["id"],
-      username: json["username"],
-    );
-  }
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
   @override
   List<Object?> get props => [id, username];
