b9695b8da7f845e78c5c72928415aaf172d95f00
Johan Castaño <jach_1997@hotmail.com>
2023-06-20 12:14:34 -0500

    302. FutureBuilder - parte 3
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/home/views/widgets/trending_list.dart
+++ lib/app/presentation/modules/home/views/widgets/trending_list.dart
@@ -6,27 +6,50 @@ import '../../../../../domain/enums.dart';
 import '../../../../../domain/failures/http_request/http_request_failure.dart';
 import '../../../../../domain/models/media/media.dart';
 import '../../../../../domain/repository/trending_repository.dart';
+import '../../../../global/utils/get_image_url.dart';
 
 typedef EitherListMedia = Either<HttpRequestFailure, List<Media>>;
 
-class TrendingList extends StatelessWidget {
+class TrendingList extends StatefulWidget {
   const TrendingList({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<TrendingList> createState() => _TrendingListState();
+}
+
+class _TrendingListState extends State<TrendingList> {
+  late final Future<EitherListMedia> _future;
+
+  @override
+  void initState() {
+    super.initState();
     final TrendingRepository repository = context.read();
+    _future = repository.getMoviesAndSeries(TimeWindow.day);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       height: 250,
       child: Center(
         child: FutureBuilder<EitherListMedia>(
-          future: repository.getMoviesAndSeries(TimeWindow.day),
+          future: _future,
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
+            if (!snapshot.hasData) {
               return CircularProgressIndicator();
             }
-            if (snapshot.hasError) {
-              return Text('Error');
-            }
+            return snapshot.data!.when(
+                left: (failure) => Text(failure.toString()),
+                right: (list) {
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      final media = list[index];
+                      return Image.network(getImageUrl(media.posterPath));
+                    },
+                    itemCount: list.length,
+                  );
+                });
             return Text(snapshot.data?.toString() ?? '');
           },
         ),
