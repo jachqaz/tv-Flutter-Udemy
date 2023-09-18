6c87799225e0df67d568b33c5a259539524bb05d
Johan Castaño <jach_1997@hotmail.com>
2023-09-17 23:27:46 -0500

    UI - movie view - parte 3
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/movie/views/widgets/movie_content.dart
+++ lib/app/presentation/modules/movie/views/widgets/movie_content.dart
@@ -1,8 +1,7 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../global/utils/get_image_url.dart';
 import '../../controller/state/movie_state.dart';
+import 'movie_header.dart';
 
 class MovieContent extends StatelessWidget {
   final MovieStateLoaded state;
@@ -15,69 +14,11 @@ class MovieContent extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Stack(
-            children: [
-              AspectRatio(
-                  aspectRatio: 16 / 13,
-                  child: ExtendedImage.network(
-                    getImageUrl(movie.backdropPath,
-                        imageQuality: ImageQuality.original),
-                    width: double.infinity,
-                    height: double.infinity,
-                    fit: BoxFit.cover,
-                  )),
-              Positioned(
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                          Colors.transparent,
-                          Colors.black54,
-                          Colors.black
-                        ])),
-                    padding: const EdgeInsets.all(15).copyWith(top: 2),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          movie.title,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Wrap(
-                          spacing: 10,
-                          children: movie.genres
-                              .map((e) => Container(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 5),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(4),
-                                        border:
-                                            Border.all(color: Colors.white)),
-                                    child: Text(
-                                      e.name,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ))
-                              .toList(),
-                        )
-                      ],
-                    ),
-                  )),
-            ],
-          ),
+          MovieHeader(movie: movie),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Text(movie.overview),
+          )
         ],
       ),
     );
---
+++ lib/app/presentation/modules/movie/views/widgets/movie_header.dart
@@ -0,0 +1,102 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../domain/models/movie/movie.dart';
+import '../../../../global/utils/get_image_url.dart';
+
+class MovieHeader extends StatelessWidget {
+  final Movie movie;
+
+  const MovieHeader({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AspectRatio(
+            aspectRatio: 16 / 13,
+            child: ExtendedImage.network(
+              getImageUrl(movie.backdropPath,
+                  imageQuality: ImageQuality.original),
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            )),
+        Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Colors.transparent,
+                    Colors.black54,
+                    Colors.black
+                  ])),
+              padding: const EdgeInsets.all(15).copyWith(top: 2),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          movie.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Wrap(
+                          spacing: 10,
+                          children: movie.genres
+                              .map((e) => Container(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 5),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        border:
+                                            Border.all(color: Colors.white)),
+                                    child: Text(
+                                      e.name,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ))
+                              .toList(),
+                        )
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: CircularProgressIndicator(
+                            value: (movie.voteAverage / 10).clamp(0.0, 1.0),
+                          )),
+                      Text(
+                        movie.voteAverage.toStringAsFixed(1),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+      ],
+    );
+  }
+}
