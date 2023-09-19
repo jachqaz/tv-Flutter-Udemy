15c6e0879d0d307a4b1486efb33d4b076ff84d59
Johan Castaño <jach_1997@hotmail.com>
2023-09-19 01:53:12 -0500

    El widget TabBarView - parte 2
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- lib/app/presentation/modules/favorites/views/favorites_view.dart
+++ lib/app/presentation/modules/favorites/views/favorites_view.dart
@@ -5,20 +5,82 @@ import '../../../global/controllers/favorites/favorites_controller.dart';
 import '../../../global/widgets/request_failed.dart';
 import 'widgets/favoritesContent.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends StatefulWidget {
   const FavoritesView({super.key});
+
+  @override
+  State<FavoritesView> createState() => _FavoritesViewState();
+}
+
+class _FavoritesViewState extends State<FavoritesView>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final FavoritesController controller = context.watch();
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        titleTextStyle: TextStyle(color: Colors.black),
+        elevation: 0,
+        title: Text('Favorites'),
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        bottom: TabBar(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          controller: _tabController,
+          indicator: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          indicatorSize: TabBarIndicatorSize.label,
+          labelColor: Colors.black,
+          tabs: [
+            SizedBox(
+              height: 30,
+              child: Tab(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text('Movies'),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+              child: Tab(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text('Series'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: controller.state.map(
         loading: (_) => Center(
           child: CircularProgressIndicator(),
         ),
         failed: (_) => RequestFail(onRetry: () => controller.init()),
-        loaded: (state) => FavoritesContent(state: state),
+        loaded: (state) => FavoritesContent(
+          state: state,
+          tabController: _tabController,
+        ),
       ),
     );
   }
--- lib/app/presentation/modules/favorites/views/widgets/favoritesContent.dart
+++ lib/app/presentation/modules/favorites/views/widgets/favoritesContent.dart
@@ -2,35 +2,17 @@ import 'package:flutter/material.dart';
 
 import '../../../../global/controllers/favorites/favorites_state.dart';
 
-class FavoritesContent extends StatefulWidget {
+class FavoritesContent extends StatelessWidget {
   final FavoritesStateLoaded state;
+  final TabController tabController;
 
-  const FavoritesContent({super.key, required this.state});
-
-  @override
-  State<FavoritesContent> createState() => _FavoritesContentState();
-}
-
-class _FavoritesContentState extends State<FavoritesContent>
-    with SingleTickerProviderStateMixin {
-  late final TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+  const FavoritesContent(
+      {super.key, required this.state, required this.tabController});
 
   @override
   Widget build(BuildContext context) {
     return TabBarView(
-      controller: _tabController,
+      controller: tabController,
       children: [
         Text('movies'),
         Text('series'),
